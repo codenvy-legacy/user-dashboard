b481d4192052b240c3f9ab327148b3a8d2cb889a
Gaurav Meena <gauravmeena0708@gmail.com>
2014-01-17 23:52:12 +0530

    making factories table dynamic

--- app/scripts/controllers/factories.js
+++ app/scripts/controllers/factories.js
@@ -18,10 +18,38 @@
 
 angular.module('odeskApp')
     .controller('FactoriesCtrl', function ($scope, $http) {
-        $scope.awesomeThings = [
+        $scope.projects = [
             {
-                'name': 'Factories',
-                'info': 'info'
+                'icon': 'images/icon-spring.png',
+                'name': 'SpringDemo',
+                'owner': 'Stevan.lemur',
+                'description': 'This project is demoing a Java application built with Spring. Displaying data in a page, submitting data and authentication sample',
+                'visibility' : 'Public',
+                'modified': 1
+            },
+            {
+                'icon': 'images/icon-angular.png',
+                'name': 'AngularDemo',
+                'owner': 'Stevan.lemur',
+                'description': 'This project is demoing a Java application built with Spring. Displaying data in a page, submitting data and authentication sample',
+                'visibility' : 'Private',
+                'modified': 2
+            },
+            {
+                'icon': 'images/icon-android.png',
+                'name': 'AndroidCodeEnvy',
+                'owner': 'Stevan.lemur',
+                'description': 'This project is demoing a Java application built with Spring. Displaying data in a page, submitting data and authentication sample',
+                'visibility' : 'Private',
+                'modified': 3
+            },
+            {
+                'icon': 'images/icon-node.png',
+                'name': 'Book Store',
+                'owner': 'Stevan.lemur',
+                'description': 'This project is demoing a Java application built with Spring. Displaying data in a page, submitting data and authentication sample',
+                'visibility' : 'Private',
+                'modified': 4
             }
         ];
     });
