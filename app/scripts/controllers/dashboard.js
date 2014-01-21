720d6ffb0617a2b98a03c3c9cacea49485aed342
Gaurav Meena <gauravmeena0708@gmail.com>
2014-01-21 14:06:59 +0530

    fixed width and minor fixes
    1. font family fix
    2. profile page fix
    3. panel fix width

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -64,6 +64,38 @@ angular.module('odeskApp')
                 'description': 'This project is demoing a Java application built with Spring. Displaying data in a page, submitting data and authentication sample',
                 'visibility' : 'Private',
                 'modified': 4
+            },
+            {
+                'icon': 'images/icon-spring.png',
+                'name': 'SpringDemo',
+                'owner': 'Stevan.lemur',
+                'description': 'This project is demoing a Java application built with Spring. Displaying data in a page, submitting data and authentication sample',
+                'visibility' : 'Public',
+                'modified': 1
+            },
+            {
+                'icon': 'images/icon-angular.png',
+                'name': 'AngularDemo',
+                'owner': 'Stevan.lemur',
+                'description': 'This project is demoing a Java application built with Spring. Displaying data in a page, submitting data and authentication sample',
+                'visibility' : 'Private',
+                'modified': 2
+            },
+            {
+                'icon': 'images/icon-android.png',
+                'name': 'AndroidCodeEnvy',
+                'owner': 'Stevan.lemur',
+                'description': 'This project is demoing a Java application built with Spring. Displaying data in a page, submitting data and authentication sample',
+                'visibility' : 'Private',
+                'modified': 3
+            },
+            {
+                'icon': 'images/icon-node.png',
+                'name': 'Book Store',
+                'owner': 'Stevan.lemur',
+                'description': 'This project is demoing a Java application built with Spring. Displaying data in a page, submitting data and authentication sample',
+                'visibility' : 'Private',
+                'modified': 4
             }
         ];
     });
