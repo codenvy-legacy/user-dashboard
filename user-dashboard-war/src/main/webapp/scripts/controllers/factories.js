ee457824319796e0e2f0f9ac1ec800fbc43be042
Gaurav Meena <gauravmeena0708@gmail.com>
2014-02-01 01:57:05 +0530

    adding mooris chart in factories

--- user-dashboard-war/src/main/webapp/scripts/controllers/factories.js
+++ user-dashboard-war/src/main/webapp/scripts/controllers/factories.js
@@ -12,11 +12,25 @@
  * Controller for factories
  */
 
-/*global angular*/
+/*global angular, Morris*/
 
 'use strict';
 
 angular.module('odeskApp')
     .controller('FactoriesCtrl', function ($scope, demoService) {
         $scope.projects = demoService;
+        setTimeout(function () {
+            Morris.Area({element: 'graph-area-line',
+                         behaveLikeLine: false,
+                         data: [{x: '2011 Q1', y: 3, z: 3},
+                                {x: '2011 Q2', y: 2, z: 1},
+                                {x: '2011 Q3', y: 2, z: 4},
+                                {x: '2011 Q4', y: 3, z: 3},
+                                {x: '2011 Q5', y: 3, z: 4}],
+                         xkey: 'x',
+                         ykeys: ['z'],
+                         labels: ['Z'],
+                         lineColors: ['#79D1CF', '#E67A77']});
+
+        }, 50);
     });
