0f793ea73895185fe27c3dfcb8b3e6eed3a238f0
Gaurav Meena <gauravmeena0708@gmail.com>
2014-02-01 03:03:01 +0530

    chart and sparkline addition in stats

--- user-dashboard-war/src/main/webapp/scripts/controllers/stats.js
+++ user-dashboard-war/src/main/webapp/scripts/controllers/stats.js
@@ -12,16 +12,26 @@
  * stats page controller
  */
 
-/*global angular*/
+/*global angular, Morris, $*/
 
 'use strict';
 
 angular.module('odeskApp')
     .controller('StatsCtrl', function ($scope, $http) {
-        $scope.awesomeThings = [
-            {
-                'name': 'Stats',
-                'info': 'info'
+        setTimeout(function () {
+            Morris.Area({element: 'graph-area-line',
+                         behaveLikeLine: false,
+                         data: [{x: '2011 Q1', y: 3, z: 3},
+                                {x: '2011 Q2', y: 2, z: 1},
+                                {x: '2011 Q3', y: 2, z: 4},
+                                {x: '2011 Q4', y: 3, z: 3},
+                                {x: '2011 Q5', y: 3, z: 4}],
+                         xkey: 'x',
+                         ykeys: ['z'],
+                         labels: ['Z'],
+                         lineColors: ['#66ccff', '#E67A77']});
+            if ($(".sparkline").length > 0) {
+                $(".sparkline").sparkline('html', { enableTagOptions: true, disableHiddenCheck: true});
             }
-        ];
+        }, 50);
     });
