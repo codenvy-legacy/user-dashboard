91ef6ba09867edf8acfe4fabe0937f7e70f47f33
Gaurav Meena <gauravmeena0708@gmail.com>
2014-03-16 13:01:43 +0000

    1. border shadow related fixes 2. chart fix and other minor fix

--- user-dashboard-war/src/main/webapp/scripts/controllers/factories.js
+++ user-dashboard-war/src/main/webapp/scripts/controllers/factories.js
@@ -15,23 +15,33 @@
 /*global angular, Morris*/
 
 'use strict';
-
 angular.module('odeskApp')
     .controller('FactoriesCtrl', function ($scope, $timeout, projectList) {
         $scope.projects = projectList.query();
         $scope.filter = {};
-        $timeout(function () {
-            Morris.Area({element: 'graph-area-line',
-                         behaveLikeLine: false,
-                         data: [{x: '2011 Q1', y: 3, z: 3},
+        var Data = [{x: '2011 Q1', y: 3, z: 3},
                                 {x: '2011 Q2', y: 2, z: 1},
                                 {x: '2011 Q3', y: 2, z: 4},
                                 {x: '2011 Q4', y: 3, z: 3},
-                                {x: '2011 Q5', y: 3, z: 4}],
+                                {x: '2011 Q5', y: 3, z: 4}];
+        $timeout(function () {
+            Morris.Line({element: 'graph-area-line',
+                         behaveLikeLine: false,
+                         data: Data,
                          xkey: 'x',
                          ykeys: ['z'],
                          labels: ['Z'],
-                         lineColors: ['#79D1CF', '#E67A77']});
+                         grid:false,
+                         lineWidth:1,
+                         smooth:false,
+                         goals:[0],
+                         goalLineColors:['#d9d9d9'],
+                         eventLineColors:['#d9d9d9'],
+                         events:[Data[0].x],
+                         pointSize:5,
+                         pointFillColors:['#ffffff'],
+                         pointStrokeColors:['#90c6ec'],
+                         lineColors: ['#e5e5e5']});
 
         });
     });
--- user-dashboard-war/src/main/webapp/scripts/controllers/stats.js
+++ user-dashboard-war/src/main/webapp/scripts/controllers/stats.js
@@ -18,18 +18,42 @@
 
 angular.module('odeskApp')
     .controller('StatsCtrl', function ($scope, $http) {
+        var Data = [{x: '2012-11-16', y: 2, z: 6},
+                                {x: '2012-11-18', y: 2, z: 4},
+                                {x: '2012-11-20', y: 3, z: 3},
+                                {x: '2012-11-24', y: 3, z: 4},
+                                {x: '2012-11-26', y: 3, z: 5},
+                                {x: '2012-11-28', y: 3, z: 6},
+                                {x: '2012-11-30', y: 3, z: 8},
+                                {x: '2012-12-01', y: 3, z: 4},
+                                {x: '2012-12-03', y: 3, z: 7},
+                                {x: '2012-12-05', y: 3, z: 10},
+                                {x: '2012-12-07', y: 3, z: 9},
+                                {x: '2012-12-09', y: 3, z: 11},
+                                {x: '2012-12-11', y: 3, z: 12},
+                                {x: '2012-12-13', y: 3, z: 13},
+                                {x: '2012-12-15', y: 3, z: 14},
+                                {x: '2012-12-18', y: 3, z: 15},
+                                ];
         setTimeout(function () {
             Morris.Area({element: 'graph-area-line',
                          behaveLikeLine: false,
-                         data: [{x: '2011 Q1', y: 3, z: 3},
-                                {x: '2011 Q2', y: 2, z: 1},
-                                {x: '2011 Q3', y: 2, z: 4},
-                                {x: '2011 Q4', y: 3, z: 3},
-                                {x: '2011 Q5', y: 3, z: 4}],
+                         data: Data,
                          xkey: 'x',
                          ykeys: ['z'],
                          labels: ['Z'],
-                         lineColors: ['#66ccff', '#E67A77']});
+                         grid:false,
+                         lineWidth:1,
+                         smooth:false,
+                         goals:[0],
+                         xLabels:'day',
+                         goalLineColors:['#d9d9d9'],
+                         eventLineColors:['#d9d9d9'],
+                         events:[Data[0].x],
+                         pointSize:5,
+                         pointFillColors:['#ffffff'],
+                         pointStrokeColors:['#90c6ec'],
+                         lineColors: ['#eff4f8']});
             if ($(".sparkline").length > 0) {
                 $(".sparkline").sparkline('html', { enableTagOptions: true, disableHiddenCheck: true});
             }
