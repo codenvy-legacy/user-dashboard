27680660aa6c60b591f179f860cd00ce9d34c5ca
Sachin Gevariya <sachin.gevariya@bacancytechnology.com>
2014-08-13 20:26:43 +0530

    Add runner tab design and get runners

--- app/scripts/controllers/runner.js
+++ app/scripts/controllers/runner.js
@@ -16,21 +16,30 @@
 angular.module('odeskApp')
     .controller('RunnerCtrl', function ($scope, $timeout, Workspace, Project, Users, $http) {
 
-      console.log(Workspace.query())
       var old_description = '';
       $scope.box = 1;
       $scope.search = 0;
       $scope.runners = [];
+      $scope.projects = [];
       $scope.ownerWorkspace = '';
       Workspace.all(function (resp) {
         $scope.workspaces = _.filter(resp, function (workspace) {return !workspace.workspaceReference.temporary;});
         angular.forEach($scope.workspaces, function (value) {
-          console.log(value)
-          /*$http({method: 'GET', url: $.map(value.workspaceReference.links,function(obj){if(obj.rel=="get projects") return obj.href})[0]}).
-              success(function (data, status) {
-                $scope.projects = $scope.projects.concat(data);
-              });*/
+
+          $http({method: 'GET', url: $.map(value.workspaceReference.links,function(obj){if(obj.rel=="get projects") return obj.href})[0]}).
+            success(function (data, status) {
+              $scope.projects = $scope.projects.concat(data);
+              angular.forEach($scope.projects, function (project) {
+                $http({method: 'GET', url:"/api/runner/"+ value.workspaceReference.id +"/processes?project="+project.path }).
+                  success(function (data, status) {
+                    if(data.length > 0){
+                      if(data[0].status == "RUNNING"){
+                        $scope.runners.push(project);
+                      }
+                    }
+                  });
+              });
+            });
         });
       });
-
     });
