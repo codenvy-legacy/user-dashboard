9093402b8caa6f20c941134675166cc71c4515c1
ta4ka <ilovethismuzic@gmail.com>
2014-08-13 17:42:34 +0200

    Manage developers: Listing of users done and setting permissions still in progress

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -17,15 +17,40 @@
 'use strict';
 
 angular.module('odeskApp')
-    .controller('DashboardCtrl', function ($scope, $timeout, Workspace, Project, Users, $http, $window) {
+    .controller('DashboardCtrl', function ($scope, $timeout, Workspace, Project, Users, Profile, $http, $window) {
         var old_description = '';
         $scope.box = 1;
         $scope.search = 0;
         $scope.projects = [];
         $scope.ownerWorkspace = '';
+        $scope.members = [];
+        
         Workspace.all(function (resp) {
             $scope.workspaces = _.filter(resp, function (workspace) {return !workspace.workspaceReference.temporary;});
             angular.forEach($scope.workspaces, function (value) {
+                var urlGetMembers = $.map(value.links, function (obj) { if (obj.rel == "get members") return obj.href })[0];
+                
+                $http({ method: 'GET', url: urlGetMembers }).
+                    success(function (data, status) {
+                        angular.forEach(data, function(item) {
+                            //var member = {};
+                            //member.roles = item.roles;
+                            //member.userId = item.userId;
+
+                            Profile.getById(item.userId).then(function (data) {
+                                var member = {
+                                    fullName: data.attributes.firstName + " " + data.attributes.lastName,
+                                    email: data.attributes.email,
+                                    userId:item.userId,
+                                    roles:item.roles
+                                };
+                                $scope.members.push(member);
+                           });
+                            
+                        });
+                    });
+                
+
                 $http({method: 'GET', url: $.map(value.workspaceReference.links,function(obj){if(obj.rel=="get projects") return obj.href})[0]}).
                     success(function (data, status) {
                         $scope.projects = $scope.projects.concat(data);
@@ -38,8 +63,55 @@ angular.module('odeskApp')
         });
         
         $scope.filter = {};
+
+        var setPermissions = function(projectPermissions,member,role) {
+            angular.forEach(projectPermissions, function (perm) {
+                if (perm.principal.type == "GROUP" && perm.principal.name == role) {
+                    member.permissions = perm.permissions;
+
+                    angular.forEach(perm.permissions, function(currentPerm) {
+                        if (currentPerm == 'read') {
+                            member.read = true;
+                        } else if (currentPerm ==  'write') {
+                            member.write = true;
+                        }
+                    });
+
+
+                } else if (perm.principal.type == "USER" && perm.principal.name == role) {
+                    console.log("to be done!");
+                }
+            });
+        };
         
         $scope.selectProject = function (project) {
+            Project.getPermissions(project.workspaceId, project.name).then(function (data) { // get the permissions for the current selected project
+                //var groupRoles;
+
+                //angular.forEach(permissions, function(permissionItem) {
+                //    if (permissionItem.principal.type == "GROUP") {
+                //        var role = {
+                //          name: permissionItem  
+                //        };
+                //    }else if (permissionItem.principal.type == "USER") {
+                        
+                //    }
+                //});
+
+                var projectPermissions = data;
+                angular.forEach($scope.members, function (member) {
+                    angular.forEach(member.roles, function (role) { // iterate users and their roles
+                        //example roles for current user:
+                        // 0: "workspace/admin"
+                        // 1: "workspace/developer"
+                        var projectPermissionsArray = $.map(projectPermissions, function (value, index) {
+                            return [value];
+                        });
+                        setPermissions(projectPermissionsArray, member, role);
+                    });
+                });
+            });
+          
           $scope.selected = project;
           old_description = project.description;
         };
