a101dec2940325a9856f5e199e96aa3c655391a8
Sachin Gevariya <sachin.gevariya@bacancytechnology.com>
2014-08-14 18:06:59 +0530

    Add runner information in runner tab

--- app/scripts/controllers/runner.js
+++ app/scripts/controllers/runner.js
@@ -16,16 +16,26 @@
 angular.module('odeskApp')
     .controller('RunnerCtrl', function ($scope, $timeout, Workspace, Project, Users, $http) {
 
-      var old_description = '';
-      $scope.box = 1;
-      $scope.search = 0;
       $scope.runners = [];
       $scope.projects = [];
-      $scope.ownerWorkspace = '';
+      $scope.ramConsumption = [];
+
       Workspace.all(function (resp) {
         $scope.workspaces = _.filter(resp, function (workspace) {return !workspace.workspaceReference.temporary;});
         angular.forEach($scope.workspaces, function (value) {
+          // Get workspace related resources
+          $http({method: 'GET', url:"/api/runner/"+ value.workspaceReference.id +"/resources" }).
+            success(function (data, status) {
 
+              var workspaceResources = {
+                workspaceName: value.workspaceReference.name,
+                usedMemory: data.usedMemory,
+                totalMemory: data.totalMemory
+              }
+              $scope.ramConsumption.push(workspaceResources);
+            });
+
+          // Get list of runners
           $http({method: 'GET', url: $.map(value.workspaceReference.links,function(obj){if(obj.rel=="get projects") return obj.href})[0]}).
             success(function (data, status) {
               $scope.projects = $scope.projects.concat(data);
@@ -34,12 +44,21 @@ angular.module('odeskApp')
                   success(function (data, status) {
                     if(data.length > 0){
                       if(data[0].status == "RUNNING"){
-                        $scope.runners.push(project);
+                        var runnerDetails = {
+                          project: project,
+                          startTime: data[0].startTime,
+                          shutdownUrl: $.map(data[0].links,function(obj){if(obj.rel=="stop") return obj.href})[0],
+                          url: $.map(data[0].links,function(obj){if(obj.rel=="web url") return obj.href})[0],
+                          dockerRecipe: $.map(data[0].links,function(obj){if(obj.rel=="runner recipe") return obj.href})[0],
+                          terminalUrl: $.map(data[0].links,function(obj){if(obj.rel=="shell url") return obj.href})[0]
+                        }
+                        $scope.runners.push(runnerDetails);
                       }
                     }
                   });
               });
             });
+
         });
       });
     });
