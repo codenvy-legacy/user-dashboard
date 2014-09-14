bdbf147e4efaed9d8ca51a104badfb90ca9a9e4d
bacancyramesh <ramesh.b@bacancytechnology.com>
2014-09-14 23:43:33 +0530

    Implemented UD-92, User Dashboard - Factory Screen

--- app/scripts/controllers/factories.js
+++ app/scripts/controllers/factories.js
@@ -17,24 +17,225 @@
 'use strict';
 angular.module('odeskApp')
     .controller('FactoriesCtrl', function ($scope, $timeout, $http, Workspace) {
+	
+		$scope.noOfDays = 30;
+		$scope.isDays = true;
+		$scope.isGraphDrawnForDays = false;
+		$scope.noOfWeeks = 12;
+		
+		$scope.selectedPeriodText = 'Last 30 Days';
+		$scope.periodTexts = ['Last 7 Days','Last 30 Days','Last 3 Months'];
+		
+		$scope.selectPeriod = function(text) {
+		
+			$scope.selectedPeriodText = text;
+			
+			if($scope.periodTexts[0] == text) {
+				$scope.noOfDays = 7;
+				$scope.isDays = true;
+				
+				timeSpanChanged();
+			}
+			if($scope.periodTexts[1] == text) {
+				$scope.noOfDays = 30;
+				$scope.isDays = true;
+				
+				timeSpanChanged();
+			}
+			if($scope.periodTexts[2] == text) {
+				$scope.noOfDays = 84;
+				$scope.noOfWeeks = 12;
+				$scope.isDays = false;
+				
+				timeSpanChanged();
+			}
+		}
+		
+		var DayData = function(sd,sdstr,ed,edstr) {
+			this.sdate=sd;
+			this.sdatestr=sdstr;
+			this.edate=ed;
+			this.edatestr = edstr;
+		};
+		
+		var getToday = function() {
+			  var currentDate = new Date();
+			  var day = currentDate.getDate();
+			  var month = currentDate.getMonth() + 1;
+			  var year = currentDate.getFullYear();
+				
+			  if(day<10) { day='0'+day; } 
+
+			  if(month<10) { month='0'+month; } 
+
+			  return year+month+day;
+		};
+		
+		var getStartDate = function() {
+			  var currentDate = new Date();
+			  var tempDate = new Date();
+			  var dateOffset = (86400000) * ($scope.noOfDays-1); // (24*60*60*1000) = 86400000 and -1 for to include today
+			  tempDate.setTime(currentDate.getTime()-dateOffset);
+			  return tempDate;
+		};
+
+		// Used to pass from date and to date in API 
+		var getDateString = function(d) {
+			var day = d.getDate();
+			  var month = d.getMonth() + 1;
+			  var year = d.getFullYear();
+				
+			  if(day<10) { day='0'+day; } 
+
+			  if(month<10) { month='0'+month; } 
+
+			  return year+month+day;
+		};
+		
+		// Used to display date on x axis of chart
+		var getDateStringDispay = function(d) {
+			var day = d.getDate();
+			  var month = d.getMonth() + 1;
+			  var year = d.getFullYear();
+				
+			  if(day<10) { day='0'+day; } 
+
+			  if(month<10) { month='0'+month; } 
+
+			  if($scope.isDays)
+				return year + '-' + month + '-' + day;
+			  else
+				return 'Week-' + year + '-' + month + '-' + day;
+			  
+		};
+		
+		// Used for graph scale
+		var getDateStringForGraph = function(d) {
+			var day = d.getDate();
+			  var month = d.getMonth() + 1;
+			  var year = d.getFullYear();
+				
+			  if(day<10) { day='0'+day; } 
+
+			  if(month<10) { month='0'+month; } 
+
+			  
+			return year + '-' + month + '-' + day;
+			  
+		};
+		
+		$scope.statsData = [];
+		$scope.fromDate = '';
+		$scope.toDate = '';
+		
+		var timeSpanChanged = function() {
+			var startDate = getStartDate();
+			var endDate = new Date();
+			
+			if($scope.isDays != $scope.isGraphDrawnForDays) // If graph drawn previously has same x unit then no need to redraw it.
+				drawMorrisGraph();
+			
+			// get from date and to date in string format
+			$scope.fromDate = getDateString(startDate);
+			$scope.toDate = getDateString(endDate);
+			
+			var steps_milliseconds = 86400000;
+			if(!$scope.isDays)
+				steps_milliseconds *= 7; // now stepping through weeks
+				
+			$scope.statsData = []; // clear the list
+			for(var date = startDate; date<=endDate;date.setTime(date.getTime()+steps_milliseconds))
+			{
+				if($scope.isDays)
+				{
+					var strDate = getDateString(date);
+					var datetemp = new Date(date.getTime());
+					$scope.statsData.push(new DayData(datetemp,strDate,datetemp,strDate));
+				}
+				else
+				{
+					var datetemp1 = new Date(date.getTime());
+					var strDate1 = getDateString(datetemp1);
+					
+					var datetemp2 = new Date(date.getTime() + (86400000*6) ); 
+					var strDate2 = getDateString(datetemp2);
+					
+					$scope.statsData.push(new DayData(datetemp1,strDate1,datetemp2,strDate2));
+				}
+			}
+	
+			clearStatsData();
+			
+			getStatsData();
+			getFactoryStats1();
+		}
+		
+		$scope.nCountFactories = 0;
+		var getStatsData = function() {
+						
+			// Build request data
+			var reqData = [];
+			
+			angular.forEach($scope.statsData, function(day,key) {
+					reqData.push({"from_date":day.sdatestr, "to_date":day.edatestr});
+				});
+					
+			
+            
+			$scope.nCountFactories = 0;
+			$scope.dailyUses = [];
+			
+            $scope.factories.forEach(function (factory){
+				var con = {
+					headers: {
+						'Content-Type': 'application/json; charset=UTF-8'
+					},
+					params:{ factory:encodeURIComponent(factory.url)},
+					cache:true
+				};
+				
+				$http.post('/api/analytics/metric/product_usage_sessions/list', reqData, con)
+                .success(function (data, status) {
+					$scope.nCountFactories++;
+					var i=0;
+					angular.forEach(data.metrics, function(day,key) {
+					    var iVal = parseInt(day.value);
+						var xVal = getDateStringForGraph($scope.statsData[i].sdate);
+						
+						var _factory = _.find($scope.dailyUses, function(ses){ if(ses.x == xVal) return ses; });
+						if(_factory)
+							_factory.z = _factory.z + iVal;
+						else
+							$scope.dailyUses.push({x:xVal,y:i,z:iVal}); 
+							
+						i++;
+					});
+					
+					if($scope.nCountFactories==$scope.factories.length){
+						// redraw chart
+						graphSessions.setData($scope.dailyUses);
+					}
+                })
+								
+			});
+		}
+		
+		var clearStatsData = function() {
+			
+		}
+	
+	
         $scope.projects = [];
-		$scope.TimeSpan = 'Last 1 Week';
 		$scope.factories = [];
 		getFactories();
         
 		var factoriesConcatUrl;
 		var factories;
-		
 
         Workspace.all(function (resp) {
 			$scope.workspaces = resp;
         });
         
-		$scope.ChangeTimeSpan = function(selection){
-			$scope.TimeSpan = selection;
-			getFactoryStats();
-		}
-		
 		function getFactoryInformation(){
 			$scope.factories.forEach(function (factory){
 				var _uri = '/api/factory/' + factory.id; 
@@ -44,26 +245,16 @@ angular.module('odeskApp')
 					_factory.description = data.description;
 					//_factory.logo = data.links[4].href;
 					_factory.created = data.created;
-					_factory.seeURL = data.links[0].href;
+					_factory.seeURL = data.links[0].href;  //TODO: invalid link
 				});
 			});
 		}
 		
 		function getFactoryStats(){
-			
-			var currentDate = new Date();
-			var toDate = new Date();
-			
-			if($scope.TimeSpan == 'Last 1 Week')
-				toDate.setDate(currentDate.getDate() - 7);
-			else if($scope.TimeSpan == 'Last 1 Month')
-				toDate.setMonth(currentDate.getMonth() - 1);
-			else if($scope.TimeSpan == 'Last 3 Months')
-				toDate.setMonth(currentDate.getMonth() - 3);
-			
+						
 			var uri = '/api/analytics/metric/factory_statistics_list';
-			uri = uri + '?from_date=' + formatDate(toDate);
-			uri = uri + '&to_date=' + formatDate(currentDate);
+			uri = uri + '?from_date=' + $scope.fromDate;
+			uri = uri + '&to_date=' + $scope.toDate;
 			
 			//uri = uri + '&' + factoriesConcatUrl;  //ToDo - Send all factories at once when 502 response is resolved.
 
@@ -83,6 +274,43 @@ angular.module('odeskApp')
 			});
 		}
 		
+		function getFactoryStats1() {
+		
+			$scope.factories.forEach(function (factory){
+				var con = {
+					headers: {
+						'Accept': 'application/json',
+						'X-Requested-With': 'XMLHttpRequest'
+					},
+					params:{ from_date:$scope.fromDate,to_date:$scope.toDate, factory:encodeURIComponent(factory.url)},
+					cache: true
+				};
+				
+				$http.get('/api/analytics/metric/factories_accepted',con)
+					.success(function (data, status, headers, config) {
+						var _factory = _.find($scope.factories, function(fct){ if(fct.id == getIDFromFactoryURL(config.params.factory)) return fct; });
+						_factory.views = data.value;
+					});
+					
+				$http.get('/api/analytics/metric/factories_built',con)
+					.success(function (data, status, headers, config) {
+						var _factory = _.find($scope.factories, function(fct){ if(fct.id == getIDFromFactoryURL(config.params.factory)) return fct; });
+						_factory.builds = data.value;
+					});
+					
+				$http.get('/api/analytics/metric/factories_run',con)
+					.success(function (data, status, headers, config) {
+						var _factory = _.find($scope.factories, function(fct){ if(fct.id == getIDFromFactoryURL(config.params.factory)) return fct; });
+						_factory.runs = data.value;
+					});
+					
+				$http.get('/api/analytics/metric/factories_imported',con)
+					.success(function (data, status, headers, config) {
+						var _factory = _.find($scope.factories, function(fct){ if(fct.id == getIDFromFactoryURL(config.params.factory)) return fct; });
+						_factory.copies = data.value;
+					});
+			});
+		}		
 		function formatDate(date){
 			var formattedDate = new String();
 
@@ -120,7 +348,8 @@ angular.module('odeskApp')
 					factoriesConcatUrl = factoriesConcatUrl.slice(0, factoriesConcatUrl.length - 8);
 				
 				getFactoryInformation();
-				getFactoryStats();
+				
+				timeSpanChanged();
 			});
 		}
 		
@@ -131,14 +360,46 @@ angular.module('odeskApp')
 			}
 		}
 		
+		
         $scope.filter = {};
-        var Data = [{x: '2011 Q1', y: 3, z: 3},
+		
+		var graphSessions = null;
+		var drawMorrisGraph = function() {
+		    $('#graph-area-line').empty();
+			graphSessions = Morris.Area({element: 'graph-area-line',
+                         behaveLikeLine: false,
+                         data: [{x:'',y:0,z:0} ],
+                         xkey: 'x',
+                         ykeys: ['z'],
+                         labels: ['Z'],
+                         grid:false,
+                         lineWidth:1,
+                         smooth:false,
+                         goals:[0],
+                         xLabels: $scope.isDays ? 'day' : 'week',
+						 xLabelFormat: function(dt) { return getDateStringDispay(dt); },
+						 /*xLabelAngle: 15,*/
+                         hoverCallback: function(index, options, content) {
+                            var row = options.data[index];
+                            return "<div class='morris-hover-row-label'>"+row.z+"</div>";
+                        },
+                         goalLineColors:['#d9d9d9'],
+                         eventLineColors:['#d9d9d9'],
+                         /*events:[Data[0].x],*/
+                         pointSize:5,
+                         pointFillColors:['#ffffff'],
+                         pointStrokeColors:['#90c6ec'],
+                         lineColors: ['#eff4f8']});
+			$scope.isGraphDrawnForDays = $scope.isDays;
+		}
+		
+        /*var Data = [{x: '2011 Q1', y: 3, z: 3},
                                 {x: '2011 Q2', y: 2, z: 1},
                                 {x: '2011 Q3', y: 2, z: 4},
                                 {x: '2011 Q4', y: 3, z: 3},
-                                {x: '2011 Q5', y: 3, z: 4}];
+                                {x: '2011 Q5', y: 3, z: 4}];*/
         $timeout(function () {
-            Morris.Line({element: 'graph-area-line',
+            /*Morris.Line({element: 'graph-area-line',
                          behaveLikeLine: false,
                          data: Data,
                          xkey: 'x',
@@ -158,7 +419,7 @@ angular.module('odeskApp')
                             var row = options.data[index];
                             return "<div class='morris-hover-row-label'>"+row.z+" Sessions</div><div class='morris-hover-point'>235 Minutes</div>";
                         },
-                         lineColors: ['#e5e5e5']});
+                         lineColors: ['#e5e5e5']});*/
 
         $(document).on( "click", ".searchfield", function() {
             $('.searchfull').show();
