80f3c4b4c2a2d72e0112e53297dc25eea5fec49e
Florent Benoit <Florent.Benoit@serli.com>
2014-12-23 14:30:33 +0100

    Add partials pages

--- Gruntfile.js
+++ Gruntfile.js
@@ -270,7 +270,7 @@ module.exports = function (grunt) {
         files: [{
           expand: true,
           cwd: '<%= yeoman.dist %>',
-          src: ['*.html', 'views/{,*/}*.html'],
+          src: ['*.html', 'views/{,*/}*.html', 'partials/{,*/}*.html'],
           dest: '<%= yeoman.dist %>'
         }]
       }
@@ -308,6 +308,7 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             '.htaccess',
             '*.html',
+            'partials/{,*/}*.html',
             'views/{,*/}*.html',
             'bower_components/**/*',
             'images/{,*/}*.{webp}',
