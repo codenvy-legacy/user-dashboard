978d1197a80fbed8facbd676a27edd907ec5ff79
Oleksii Orel <oorel@codenvy.com>
2015-01-27 10:42:30 +0200

    UD-324: add credit card

---
+++ app/scripts/controllers/billing.js
@@ -0,0 +1,32 @@
+/*jslint
+ browser: true,
+ devel:true ,
+ node:true,
+ nomen: true,
+ es5:true
+ */
+
+/**
+ * @author Oleksii Orel
+ * @date 23/01/2015
+ * Controller for runners
+ */
+
+'use strict';
+angular.module('odeskApp')
+    .controller('BillingCtrl', function ($scope, Countries) {
+
+        $scope.creditCards = [];
+        $scope.countries = Countries.all();
+        $scope.country = Countries.default();
+
+
+
+
+        var init = function () {
+            $scope.creditCards = [];
+
+        };
+        init();
+    }
+);
