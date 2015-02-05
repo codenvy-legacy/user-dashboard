164ba3844a645e2f881d141c9ccb25d6e61320d5
Viktor Kuznyetsov <vkuznyetsov@codenvy.com>
2015-02-05 14:48:12 +0200

    Update docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -6,6 +6,6 @@
 # sudo groupadd docker
 # sudo gpasswd -a ${USER} docker
 # log out and log back in again.
-docker run -i -t -e USRID=$(id -u) -e USRGR=$(id -g) -v $(pwd):/home/user/app -v ${HOME}/.m2:/home/user/.m2 vkuznyetsov/odyssey sh -c 'sudo chown user:user -R /home/user/app && sudo chown user:user -R /home/user/.m2 && mvn clean install; sudo chown $USRID:$USRGR -R /home/user/app; sudo chown $USRID:$USRGR -R /home/user/.m2'
+docker run -i -t -e USRID=$(id -u) -e USRGR=$(id -g) -v $(pwd):/home/user/app -v ${HOME}/.m2:/home/user/.m2 vkuznyetsov/user-dashboard sh -c 'sudo chown user:user -R /home/user/app && sudo chown user:user -R /home/user/.m2 && mvn clean install; sudo chown $USRID:$USRGR -R /home/user/app; sudo chown $USRID:$USRGR -R /home/user/.m2'
 
 
