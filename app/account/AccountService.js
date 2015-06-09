a27b9025069917575ee5eccaa6a268a6181535ac
Ann Shumilova <ashumilova@codenvy.com>
2015-06-09 11:14:01 +0300

    UD-577: adapt subscriptions, billing and account resources.

--- app/account/AccountService.js
+++ app/account/AccountService.js
@@ -22,9 +22,11 @@ angular.module('odeskApp')
         AccountService.SAAS_PREPAID_PLAN_ID = "prepaid";
         AccountService.ONPREMISES_SERVICE_ID = "OnPremises";
         AccountService.RESOURCES_LOCKED_PROPERTY = "codenvy:resources_locked";
+        AccountService.PAYMENT_LOCKED_PROPERTY = "codenvy:payment_locked";
         AccountService.subscriptions = [];
         AccountService.accounts = [];
-        AccountService.resources = {};
+        AccountService.usedResources = {};
+        AccountService.providedResources = {};
         AccountService.accountDetails = {};
 
         //Get all accounts, where the current user has membership:
@@ -59,27 +61,6 @@ angular.module('odeskApp')
                 });
                 deferred.resolve(accounts);
             });
-            return deferred.promise;
-        };
-
-        //Get all accounts, where the current user has pointed role:
-        AccountService.getAccountResources = function (accountId) {
-            var deferred = $q.defer();
-            var con = {
-                headers: {
-                    'Accept': 'application/json',
-                    'X-Requested-With': 'XMLHttpRequest'
-                }
-            };
-            $http.get('/api/account/' + accountId + "/resources", con)
-                .success(function (data) {
-                    AccountService.resources = data;
-                    deferred.resolve(data); //resolve data
-                })
-                .error(function (err) {
-                    deferred.reject();
-                });
-
             return deferred.promise;
         };
 
@@ -123,27 +104,54 @@ angular.module('odeskApp')
             return deferred.promise;
         };
 
+        //Get all accounts, where the current user has pointed role:
+        AccountService.getUsedResources = function (accountId) {
+            var deferred = $q.defer();
+            var con = {
+                headers: {
+                    'Accept': 'application/json',
+                    'X-Requested-With': 'XMLHttpRequest'
+                }
+            };
+            $http.get('/api/saas/resources/' + accountId + "/used", con)
+                .success(function (data) {
+                    AccountService.usedResources = data;
+                    deferred.resolve(data); //resolve data
+                })
+                .error(function (err) {
+                    deferred.reject();
+                });
+
+            return deferred.promise;
+        };
+
         AccountService.getUsedMemory = function (resources) {
-            var saasSubscription = _.find(resources, function (subscription) {
-                return subscription.subscriptionReference.serviceId == AccountService.SAAS_SERVICE_ID;
-            });
-            var used = _.pluck(saasSubscription.used, "memory");
+            var used = _.pluck(resources, "memory");
             var usedMb = used.reduce(function(sum, use) {
                 sum += use;
                 return sum;
             });
-            //return (usedMb / 1024 / 60).toFixed(2);
             return (usedMb).toFixed(2);
         };
 
-        AccountService.getPrepaidGbH = function (subscriptions) {
-            var saasSubscription = _.find(subscriptions, function (subscription) {
-                return subscription.serviceId == AccountService.SAAS_SERVICE_ID;
-            });
-            if(!saasSubscription.properties || !saasSubscription.properties.PrepaidGbH){
-                return 0;
-            }
-            return saasSubscription.properties.PrepaidGbH;
+        AccountService.getProvidedResources = function (accountId) {
+            var deferred = $q.defer();
+            var con = {
+                headers: {
+                    'Accept': 'application/json',
+                    'X-Requested-With': 'XMLHttpRequest'
+                }
+            };
+            $http.get('/api/saas/resources/' + accountId + "/provided", con)
+                .success(function (data) {
+                    AccountService.providedResources = data;
+                    deferred.resolve(data); //resolve data
+                })
+                .error(function (err) {
+                    deferred.reject();
+                });
+
+            return deferred.promise;
         };
 
         //Get list of subscriptions for pointed accounts:
@@ -174,7 +182,7 @@ angular.module('odeskApp')
                     'X-Requested-With': 'XMLHttpRequest'
                 }
             };
-            $http.get('/api/account/' + accountId + '/subscriptions', con)
+            $http.get('/api/subscription/account/' + accountId , con)
                 .success(function (data) {
                     AccountService.subscriptions = data;
                     deferred.resolve(data);
@@ -246,7 +254,7 @@ angular.module('odeskApp')
         //Remove subscription by it's ID:
         AccountService.removeSubscription = function (subscriptionId) {
             var deferred = $q.defer();
-            $http.delete('/api/account/subscriptions/' + subscriptionId)
+            $http.delete('/api/subscription/' + subscriptionId)
                 .success(function (data) {
                     deferred.resolve(data); //resolve data
                 })
@@ -268,7 +276,7 @@ angular.module('odeskApp')
             };
 
             var deferred = $q.defer();
-            $http.post('/api/account/subscriptions/', data, con)
+            $http.post('/api/subscription/', data, con)
                 .success(function (data) {
                     deferred.resolve(data); //resolve data
                 })
